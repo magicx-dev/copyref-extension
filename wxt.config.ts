f29b8d4e6d126533140dda8c2170ca71508c8364
Simon Mau <git@simonmau.me>
2025-08-20 23:14:39 +0800

    feat: Add TaiwindCSS to WXT

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'wxt';
+import tailwindcss from "@tailwindcss/vite";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-vue'],
   srcDir: "src",
   publicDir: "src/public",
+  vite: () => ({
+    plugins: [tailwindcss()],
+  }),
 });

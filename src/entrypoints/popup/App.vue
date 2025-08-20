e70757e25c31b48fbbb6cafcb8e38c08bfe8c86d
Simon Mau <git@simonmau.me>
2025-08-21 01:32:54 +0800

    feat: Add first version

--- src/entrypoints/popup/App.vue
+++ src/entrypoints/popup/App.vue
@@ -1,30 +1,60 @@
 <script lang="ts" setup>
-import HelloWorld from '@/components/HelloWorld.vue';
+import { onMounted, ref } from 'vue';
+import { Textarea } from '@/components/ui/textarea'
+import Button from '@/components/ui/button/Button.vue';
+import { CopyCheck } from 'lucide-vue-next';
+import { getCurrentTab } from '@/lib/tab-utils';
+
+let data = ref('')
+let countdown = ref(3)
+let interval: NodeJS.Timeout;
+
+const clearInterval = (interval: NodeJS.Timeout) => {
+  window.clearInterval(interval);
+};
+
+onMounted(async () => {
+  setTimeout(async () => {
+    const currentTab = await getCurrentTab()
+    console.log(currentTab)
+    if (!currentTab) return
+
+    const htmlData = `<a href="${currentTab.url}">${currentTab.title}</a>`;
+    const markdownData = `[${currentTab.title}](${currentTab.url})`;
+    data.value = markdownData
+
+    navigator.clipboard.write([
+      new ClipboardItem({
+        "text/html": new Blob([htmlData], { type: "text/html" }),
+        "text/plain": new Blob([markdownData], { type: "text/plain" })
+      })
+    ]);
+
+    interval = setInterval(() => {
+      countdown.value--
+      if (countdown.value === 0) {
+        window.close()
+      }
+    }, 1000)
+
+  }, 1);
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://wxt.dev" target="_blank">
-      <img src="/wxt.svg" class="logo" alt="WXT logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="@/assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="px-5 py-5">
+    <div class="text-right ">
+      <span class="text-slate-300">{{ countdown }}</span>
+    </div>
+    <div class="flex items-center justify-center space-x-3 text-green-500">
+      <CopyCheck class="size-10" />
+      <span class="text-lg">{{ i18n.t('popup_copy_successful') }}</span>
+    </div>
+    <div class="mt-6 grid w-full gap-2">
+      <Textarea class="text-sm" :model-value="data" @focus="clearInterval(interval)" />
+      <Button disabled>{{ i18n.t('popup_copied_to_clipboard') }}</Button>
+    </div>
   </div>
-  <HelloWorld msg="WXT + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #54bc4ae0);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
--- wxt.config.ts
+++ wxt.config.ts
@@ -3,13 +3,19 @@ import tailwindcss from "@tailwindcss/vite";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-vue'],
+  modules: [
+    '@wxt-dev/module-vue',
+    '@wxt-dev/i18n/module',
+  ],
   srcDir: "src",
   publicDir: "src/public",
   vite: () => ({
     plugins: [tailwindcss()],
   }),
   manifest: {
+    "name": "__MSG_ext_name__",
+    "description": "__MSG_ext_desc__",
+    'default_locale': 'en',
     "commands": {
       "copy": {
         "suggested_key": {
@@ -17,7 +23,7 @@ export default defineConfig({
           "mac": "MacCtrl+Shift+C",
           "windows": "Ctrl+Shift+C"
         },
-        "description": "Copy Markdown Format Web Addresses."
+        "description": "__MSG_command_copy_desc__",
       }
     },
   }
